bde7ae61bf6a76144f8b4de5fddf75a5e96219af
SaToRyOyA <hj-g19009@sist.ac.jp>
2021-05-13 22:35:54 +0900

    5/13　ゲーム処理プログラムのヘッダーとソースの追加 　　　経過時間管理クラス、FPS関連関数の実装

---
+++ SakuhinⅣ/GameProc.cpp
@@ -0,0 +1,46 @@
+#include "DxLib.h"
+#include "define.h"
+#include "enum.h"
+#include "class.h"
+#include "variable.h"
+#include "GameProcHeader.h"
+
+int StartTimeFps;
+int CountFps;
+float CalcFps;
+int SampleNumFps = GAME_FPS;
+
+TIME_COUNT TimeCounter;
+
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0)
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps)
+	{
+		int now = GetNowCount();
+
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	TimeCounter.UPDATE();
+
+	return;
+}
+
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;
+	int waitTime = CountFps * 1000 / GAME_FPS - resultTime;
+
+	if (waitTime > 0)
+	{
+		WaitTimer(waitTime);
+	}
+	return;
+}
---
+++ SakuhinⅣ/GameProcHeader.h
@@ -0,0 +1,46 @@
+#include "DxLib.h"
+#include "define.h"
+#include "enum.h"
+#include "class.h"
+#include "variable.h"
+
+//--------経過時間カウントクラス--------
+class TIME_COUNT
+{
+private:int TimeCou = 0;
+private:BOOL CountFlag = false;
+
+public:
+	   //----カウント初期化----
+	   VOID RESET(VOID)
+	   {
+		   TimeCou = 0;
+		   return;
+	   }
+	   //----カウント開始----
+	   VOID START(VOID)
+	   {
+		   CountFlag = TRUE;
+		   return;
+	   }
+	   //----現在の経過時間----
+	   int NOW(VOID)
+	   {
+		   return(TimeCou / GAME_FPS);
+	   }
+	   //----カウント停止----
+	   VOID STOP(VOID)
+	   {
+		   CountFlag = FALSE;
+		   return;
+	   }
+	   //----カウント---
+	   VOID UPDATE(VOID)
+	   {
+		   if (CountFlag)TimeCou++;
+		   return;
+	   }
+};
+
+VOID MY_FPS_UPDATE(VOID);
+VOID MY_FPS_WAIT(VOID);
