4cceb0eab53753520b7f4205baa919cddf311c04
HRXARIA <hj-g19026@sist.ac.jp>
2021-05-19 15:39:01 +0900

    5/19 image.hの追加←class.hからIMAGE引き抜いた 　　BGM.ｈの変更←class.hからMUSIC引き抜いた

--- SakuhinⅣ/BGM.h
+++ SakuhinⅣ/BGM.h
@@ -1,6 +1,16 @@
+#include "define.h"
 //BGM
 BGM BGM_TITLE;
 BGM BGM_PLAY;
 BGM BGM_COMP;
 BGM BGM_END;
 BGM BGM_SE;
+
+//âπê∫
+class MUSIC
+{
+public:
+	char path[PATH_MAX];
+	int handle;
+	int handle2;
+};
