27929d5b9399c46f98fd288dc48dd80f0c29593b
oobashuhe <hj-g19004@sist.ac.jp>
2021-05-25 13:32:22 +0900

    BGM.hないのMUSICクラスの処理を追加

--- SakuhinⅣ/BGM.h
+++ SakuhinⅣ/BGM.h
@@ -1,4 +1,5 @@
-#include"class.h"
+#include "define.h"
+#include "Dxlib.h"
 
 //BGM
 MUSIC BGM_TITLE;
@@ -6,3 +7,39 @@ MUSIC BGM_PLAY;
 MUSIC BGM_COMP;
 MUSIC BGM_END;
 MUSIC BGM_SE;
+
+class MUSIC
+{
+public:
+	int x;
+	int y;
+
+private:
+	int MusicHandle;
+	char path[PATH_MAX];
+
+public:
+	VOID SetHandle(int Date)
+	{
+		MusicHandle = Date;
+		return;
+	}
+	VOID DeleteHandle(VOID)
+	{
+		DeleteMusicMem(MusicHandle);
+		return;
+	}
+	VOID SetPath(char MusicPath[])
+	{
+		strcpy(MusicPath, MusicPath);
+		return;
+	}
+	int GetHandle(VOID)
+	{
+		return(MusicHandle);
+	}
+	char* GetPath(VOID)
+	{
+		return(path);
+	}
+};
