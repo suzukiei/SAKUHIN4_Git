77154c736267ab3a65e0603fefd95998f778bc61
EISAN_ <hj-g19015@sist.ac.jp>
2021-06-02 14:17:11 +0900

    6/2 TEXTEVENT関係

---
+++ SakuhinⅣ/TextEvent.cpp
@@ -0,0 +1,20 @@
+#include "DxLib.h"
+#include "class.h"
+#include "define.h"
+#include "enum.h"
+#include "flag.h"
+#include "menu.h"
+#include "movement.h"
+#include "textevent.h"
+
+VOID TEXTEVENT(VOID)
+{
+	switch (textNumber) {
+	case TEXT_OP:
+		//DrawString();
+		//DrawGraph();
+		//‚Ü‚¾‚Ü‚¾’Ç‰Á—\’è
+		break;
+	}
+	return;
+}
--- SakuhinⅣ/textevent.h
+++ SakuhinⅣ/textevent.h
@@ -1,2 +1,11 @@
 #define TEXT_POSITION_X 380 //テキストポジションXの値
 #define TEXT_POSITION_Y 300 //テキストポジションYの値
+
+int textNumber;
+
+enum text {
+	TEXT_OP,
+	TEXT_OP_1//まだまだ追加予定
+};
+
+VOID TEXTEVENT(VOID);
