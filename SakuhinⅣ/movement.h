76be8ad9d5099662b007bf6ea481f6dad67e3143
EISAN_ <hj-g19015@sist.ac.jp>
2021-06-04 11:46:08 +0900

    6/4キャラの当たり判定を制作

--- SakuhinⅣ/movement.h
+++ SakuhinⅣ/movement.h
@@ -7,7 +7,8 @@
 BOOL MOVEMENT(CHARA*,int);				//キャラの移動関数 //移動できたときTRUE
 VOID MY_ALL_KEYDOWN_UPDATE(VOID);	//キーの入力状態を更新する関数
 BOOL CHARA_COLLISION(CHARA,MAP[MAP_DIV_HEIGHT][MAP_DIV_WIDTH]); //当たり判定を確認する関数の定義 //壁などに衝突しているときTRUE
-
+VOID CHECK_COLLISION_GOAL(VOID);
+BOOL MY_CHECK_RECT_COLL(RECT, RECT);	//領域の当たり判定をする関数
 
 //キーボードの入力を取得
 char AllKeyState[KEY_CODE_KIND] = { '\0' };		//すべてのキーの状態が入る
@@ -17,6 +18,7 @@ char OldAllKeyState[KEY_CODE_KIND] = { '\0' };	//すべてのキーの状態(直前)が入る
 BOOL MY_KEY_DOWN(int);				//キーを押しているか、キーコードで判断する 
 BOOL MY_KEY_UP(int);				//キーを押し上げたか、キーコードで判断する
 
+CHARA player;
 enum MOVE {
 	UP,
 	DOWN,
