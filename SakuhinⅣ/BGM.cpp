7b8504a87db31c78429adf8316d4340574f3aadb
oobashuhe <hj-g19004@sist.ac.jp>
2021-06-07 20:02:38 +0900

    BGM.cpp内にダミーの処理追加

--- SakuhinⅣ/BGM.cpp
+++ SakuhinⅣ/BGM.cpp
@@ -88,60 +88,62 @@ VOID SE_BGM(VOID)
 }
 
 //BGM読み込み処理
-//BOOL MY_LOAD_MUSIC(VOID)
-//{
-//	BGM_TITLE.SetPath(MUSIC_BGM_PATH_TITLE);
-//	BGM_TITLE.SetHandle = LoadSoundMem(BGM_TITLE.SetPath);
-//	if (BGM_TITLE.SethHandle == -1)
-//	{
-//		MessageBox(GetMainWindowHandle(), MUSIC_BGM_PATH_TITLE, MUSIC_LOAD_ERR_TITLE, MB_OK);
-//		return(FALSE);
-//	}
-//
-//
-//	BGM_PLAY.SetPath(MUSIC_BGM_PATH);
-//		BGM_PLAY.SetHandle = LoadSoundMem(BGM_PLAY.SetPath);
-//	if (BGM_PLAY.SethHandle == -1)
-//	{
-//		MessageBox(GetMainWindowHandle(), MUSIC_BGM_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
-//		return(FALSE);
-//	}
-//
-//	BGM_COMP.SetPath(MUSIC_BGM_COMP_PATH);
-//		BGM_COMP.SetHandle = LoadSoundMem(BGM_COMP.SetPath);
-//	if (BGM_COMP.SethHandle == -1)
-//	{
-//		MessageBox(GetMainWindowHandle(), MUSIC_BGM_COMP_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
-//		return(FALSE);
-//	}
-//
-//	BGM_END.SetPath(MUSIC_BGM_FAIL_PATH);
-//		BGM_END.SetHandle = LoadSoundMem(BGM_END.SetPath);
-//	if (BGM_END.SethHandle == -1)
-//	{
-//		MessageBox(GetMainWindowHandle(), MUSIC_BGM_FAIL_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
-//		return(FALSE);
-//	}
-//
-//	BGM_SE.SetPath(MUSIC_BGM_SE_PATH);
-//	BGM_SE.SetHandle = LoadSoundMem(BGM_SE.SetPath);
-//	if (BGM_SE.SethHandle == -1)
-//	{
-//		MessageBox(GetMainWindowHandle(), MUSIC_BGM_SE_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
-//		return(FALSE);
-//	}
-//
-//	return TRUE;
-//}
-//
+BOOL MY_LOAD_MUSIC(VOID)
+{
+	//BGM_TITLE.SetPath(MUSIC_BGM_PATH_TITLE);
+	//BGM_TITLE.SetHandle = LoadSoundMem(BGM_TITLE.SetPath);
+	//if (BGM_TITLE.SethHandle == -1)
+	//{
+	//	MessageBox(GetMainWindowHandle(), MUSIC_BGM_PATH_TITLE, MUSIC_LOAD_ERR_TITLE, MB_OK);
+	//	return(FALSE);
+	//}
+
+
+	//BGM_PLAY.SetPath(MUSIC_BGM_PATH);
+	//	BGM_PLAY.SetHandle = LoadSoundMem(BGM_PLAY.SetPath);
+	//if (BGM_PLAY.SethHandle == -1)
+	//{
+	//	MessageBox(GetMainWindowHandle(), MUSIC_BGM_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
+	//	return(FALSE);
+	//}
+
+	//BGM_COMP.SetPath(MUSIC_BGM_COMP_PATH);
+	//	BGM_COMP.SetHandle = LoadSoundMem(BGM_COMP.SetPath);
+	//if (BGM_COMP.SethHandle == -1)
+	//{
+	//	MessageBox(GetMainWindowHandle(), MUSIC_BGM_COMP_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
+	//	return(FALSE);
+	//}
+
+	//BGM_END.SetPath(MUSIC_BGM_FAIL_PATH);
+	//	BGM_END.SetHandle = LoadSoundMem(BGM_END.SetPath);
+	//if (BGM_END.SethHandle == -1)
+	//{
+	//	MessageBox(GetMainWindowHandle(), MUSIC_BGM_FAIL_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
+	//	return(FALSE);
+	//}
+
+	//BGM_SE.SetPath(MUSIC_BGM_SE_PATH);
+	//BGM_SE.SetHandle = LoadSoundMem(BGM_SE.SetPath);
+	//if (BGM_SE.SethHandle == -1)
+	//{
+	//	MessageBox(GetMainWindowHandle(), MUSIC_BGM_SE_PATH, MUSIC_LOAD_ERR_TITLE, MB_OK);
+	//	return(FALSE);
+	//}
+
+	//ダミー
+	return TRUE;
+}
+
 //BGM削除処理
-//BOOL MY_DELETE_MUSIC(VOID)
-//{
-//	DeleteSoundMem(BGM_TITLE.MusicHandle);
-//	DeleteSoundMem(BGM_PLAY.MusicHandle);
-//	DeleteSoundMem(BGM_COMP.MusicHandle);
-//	DeleteSoundMem(BGM_END.MusicHandle);
-//	DeleteSoundMem(BGM_SE.MusicHandle);
-//
-//	return;
-//}
+BOOL MY_DELETE_MUSIC(VOID)
+{
+	//DeleteSoundMem(BGM_TITLE.MusicHandle);
+	//DeleteSoundMem(BGM_PLAY.MusicHandle);
+	//DeleteSoundMem(BGM_COMP.MusicHandle);
+	//DeleteSoundMem(BGM_END.MusicHandle);
+	//DeleteSoundMem(BGM_SE.MusicHandle);
+
+	//ダミー
+	return TRUE;
+}
